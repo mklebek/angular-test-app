ca0d49651333928774969a801e0e627f22747afe
Michal Klebek <michal.klebek@gmail.com>
2022-04-23 09:41:01 +0200

    Import material button and input modules

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -5,15 +5,19 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   exports: [
     MatIconModule,
     MatListModule,
     MatCardModule,
+    MatInputModule,
     MatFormFieldModule,
     MatSidenavModule,
     MatToolbarModule,
+    MatButtonModule,
   ],
 })
 export class MaterialModule {}
