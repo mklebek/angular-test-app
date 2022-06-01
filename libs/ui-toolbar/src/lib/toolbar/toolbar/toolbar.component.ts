0dbc91241092eceee36cf755bed6c4a5614d47cc
Michal Klebek <michal.klebek@gmail.com>
2022-06-01 11:36:25 +0200

    Set basic toolbar

--- libs/ui-toolbar/src/lib/toolbar/toolbar/toolbar.component.ts
+++ libs/ui-toolbar/src/lib/toolbar/toolbar/toolbar.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'src-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss'],
 })
-export class ToolbarComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ToolbarComponent {
+  @Output() logout = new EventEmitter();
+  @Output() toggleSidenav = new EventEmitter();
 }
--- libs/ui-toolbar/src/lib/ui-toolbar.module.ts
+++ libs/ui-toolbar/src/lib/ui-toolbar.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToolbarComponent } from './toolbar/toolbar/toolbar.component';
+import { MaterialModule } from '@src/material';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MaterialModule],
+  exports: [ToolbarComponent],
   declarations: [ToolbarComponent],
 })
 export class UiToolbarModule {}
